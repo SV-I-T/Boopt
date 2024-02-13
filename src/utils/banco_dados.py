46524ac24b6857d8cff2d7b5e6190c4bde04b6cd
Marco <marco@sucessoemvendas.com.br>
2024-02-13 14:37:05 -0300

    função facilitadora

--- src/utils/banco_dados.py
+++ src/utils/banco_dados.py
@@ -1,7 +1,10 @@
-import os
-
 from dotenv import load_dotenv
 from flask_pymongo import PyMongo
+from pymongo.collection import Collection
 
 load_dotenv()
 mongo = PyMongo()
+
+
+def db(colecao: str, tabela: str) -> Collection:
+    return mongo.cx[colecao][tabela]
