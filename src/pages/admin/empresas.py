e6625cd8bb5264e04f19152efab997bc868d0784
Marco <marco@sucessoemvendas.com.br>
2024-02-13 14:38:32 -0300

    empresas atualizado

--- src/pages/admin/empresas.py
+++ src/pages/admin/empresas.py
@@ -1,8 +1,82 @@
 import dash_mantine_components as dmc
-from dash import register_page
+from dash import (
+    ClientsideFunction,
+    Input,
+    Output,
+    State,
+    callback,
+    clientside_callback,
+    html,
+    no_update,
+    register_page,
+)
+from dash.exceptions import PreventUpdate
+from dash_iconify import DashIconify
+from utils.modelo_empresa import Empresa
+from utils.modelo_usuario import checar_login
 
 register_page(__name__, path="/admin/empresas", title="Gerenciar Empresas")
 
+SEGMENTOS_PADROES = sorted(["Farmácia", "Eletromóveis", "Automóveis", "Imóveis"])
 
+
+def moval_nova_empresa():
+    return dmc.Modal(
+        id="modal-nova-empresa",
+        title=dmc.Title("Nova Empresa", weight=600, order=2),
+        zIndex=10000,
+        size=600,
+        children=[
+            dmc.TextInput(id="nome-nova-empresa", label="Nome", required=True),
+            dmc.Select(
+                id="segmento-nova-empresa",
+                label="Segmento",
+                data=SEGMENTOS_PADROES,
+                creatable=True,
+                searchable=True,
+                clearable=True,
+            ),
+            dmc.TransferList(),
+            dmc.Button(id="btn-criar-nova-empresa", children="Criar"),
+            html.Div(id="feedback-modal-nova-empresa"),
+        ],
+    )
+
+
+@checar_login
 def layout():
-    return [dmc.Title("Gerenciamento de empresas", order=1, weight=700)]
+    return [
+        dmc.Title("Gerenciamento de empresas", order=1, weight=700),
+        dmc.Button(
+            id="btn-modal-nova-empresa",
+            children="Nova empresa",
+            leftIcon=DashIconify(icon="fluent:add-12-filled", width=24),
+            variant="gradient",
+        ),
+        moval_nova_empresa(),
+    ]
+
+
+clientside_callback(
+    ClientsideFunction(namespace="clientside", function_name="abrir_modal"),
+    Output("modal-nova-empresa", "opened"),
+    Input("btn-modal-nova-empresa", "n_clicks"),
+)
+
+
+@callback(
+    Output("modal-nova-empresa", "opened", allow_duplicate=True),
+    Output("feedback-modal-nova-empresa", "children"),
+    Input("btn-criar-nova-empresa", "n_clicks"),
+    State("nome-nova-empresa", "value"),
+    State("segmento-nova-empresa", "value"),
+    prevent_initial_call=True,
+)
+def criar_empresa(n, nome: str, segmento: str):
+    if not n:
+        raise PreventUpdate
+    try:
+        empresa = Empresa(nome=nome, segmento=segmento)
+        empresa.registrar()
+    except AssertionError as e:
+        return no_update, dmc.Alert(color="red", variant="filled", children=str(e))
--- src/utils/modelo_empresa.py
+++ src/utils/modelo_empresa.py
@@ -1,8 +1,28 @@
+from typing import Any, Optional
+
 from bson import ObjectId
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
+from utils.banco_dados import mongo
 
 
 class Empresa(BaseModel):
+    id_: Optional[ObjectId] = Field(alias="_id", default=None)
     nome: str
-    segmento: str
-    gestor_id: list[ObjectId]
+    segmento: Optional[str] = None
+    gestores: Optional[list[ObjectId]] = None
+
+    class Config:
+        str_strip_whitespace = True
+
+    @field_validator("nome", mode="before")
+    @classmethod
+    def em_branco(cls, v: Any, field) -> Any:
+        assert bool(v), "Todos os campos marcados com (*) são obrigatórios"
+        return v
+
+    def registrar(self) -> None:
+        assert not mongo.cx["Boopt"]["Empresas"].find_one(
+            {"nome": self.nome}
+        ), "Já existe uma empresa come esse nome."
+        r = mongo.cx["Boopt"]["Empresas"].insert_one(self.model_dump())
+        assert r.acknowledged, "Ocorreu algo de errado. Tente novamente mais tarde."
