c6ad150b6507fbc719f8bb88f5227230c35c3161
Marco <marco@sucessoemvendas.com.br>
2024-02-20 17:31:25 -0300

    área de dashboard iniciada

---
+++ src/pages/settings/dashboard.py
@@ -0,0 +1,11 @@
+import dash_mantine_components as dmc
+from dash import register_page
+from flask_login import current_user
+from utils.modelo_usuario import Usuario, checar_login
+
+register_page(__name__, path="/dashboard", title="Dashboard")
+
+
+@checar_login
+def layout():
+    return [dmc.Title("Minha Boopt", order=1, weight=500)]
