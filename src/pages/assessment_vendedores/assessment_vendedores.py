a182112fd941313e7462407427dd994fd448ac1f
Marco <marco@sucessoemvendas.com.br>
2024-02-21 17:40:55 -0300

    pagina inicial de assessment vendedor criada

--- src/pages/assessment_vendedores/assessment_vendedores.py
+++ src/pages/assessment_vendedores/assessment_vendedores.py
@@ -1,12 +1,17 @@
 from dash import register_page
+from flask_login import current_user
+from utils.banco_dados import db
+from utils.modelo_usuario import Usuario, checar_login
 
 register_page(
     __name__,
     path="/assessment-vendedor",
-    title="Assessment",
+    title="Assessment Vendedor",
     redirect_from=["/dashboard"],
 )
 
 
+@checar_login
 def layout():
+    usr: Usuario = current_user
     return None
