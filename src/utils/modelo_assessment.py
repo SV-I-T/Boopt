2681a6f07e6395c498c32a8e779da2fe1fd5d22e
Marco <marco@sucessoemvendas.com.br>
2024-03-06 15:02:20 -0300

    funções facilitadoras para buscar na base de dados

--- src/utils/modelo_assessment.py
+++ src/utils/modelo_assessment.py
@@ -24,3 +24,109 @@ class AssessmentVendedor(BaseModel):
             self.model_dump(exclude={"id_"})
         )
         assert r.acknowledged, "Ocorreu algo de errado. Tente novamente mais tarde."
+
+    @classmethod
+    def resultado(cls, id_resposta: ObjectId) -> dict | None:
+        r = db("AssessmentVendedores", "Respostas").aggregate(
+            [
+                {"$match": {"_id": ObjectId(id_resposta)}},
+                {
+                    "$lookup": {
+                        "from": "Aplicações",
+                        "localField": "id_aplicacao",
+                        "foreignField": "_id",
+                        "as": "aplicacao",
+                    }
+                },
+                {
+                    "$lookup": {
+                        "from": "Formulários",
+                        "localField": "aplicacao.id_form",
+                        "foreignField": "_id",
+                        "as": "form",
+                    }
+                },
+                {"$unwind": {"path": "$aplicacao", "preserveNullAndEmptyArrays": True}},
+                {"$unwind": {"path": "$form", "preserveNullAndEmptyArrays": True}},
+                {
+                    "$project": {
+                        "_id": 0,
+                        "notas": 1,
+                        "form": {
+                            "competencias": {
+                                "nome": 1,
+                                "frases": {"id": 1, "notas": 1},
+                            },
+                            "etapas": 1,
+                        },
+                    },
+                },
+            ]
+        )
+
+        if r.alive:
+            return r.next()
+        else:
+            return None
+
+    @classmethod
+    def buscar_ultimo(cls, id_usr: ObjectId) -> dict | None:
+        r = db("AssessmentVendedores", "Aplicações").aggregate(
+            [
+                {"$unwind": "$participantes"},
+                {"$match": {"participantes": id_usr}},
+                # Buscar somente a última aplicação para o usuário
+                {"$sort": {"_id": -1}},
+                {"$limit": 1},
+                {
+                    "$lookup": {
+                        "from": "Respostas",
+                        "let": {
+                            "id_aplicacao": "$_id",
+                            "participantes": "$participantes",
+                        },
+                        "pipeline": [
+                            {
+                                "$match": {
+                                    "$expr": {
+                                        "$and": [
+                                            {"$eq": ["$id_usuario", "$$participantes"]},
+                                            {
+                                                "$eq": [
+                                                    "$id_aplicacao",
+                                                    "$$id_aplicacao",
+                                                ]
+                                            },
+                                        ]
+                                    }
+                                }
+                            },
+                            # Buscar somenta a última resposta
+                            {"$sort": {"_id": -1}},
+                            {"$limit": 1},
+                            {
+                                "$project": {
+                                    "id_aplicacao": 0,
+                                    "id_usuario": 0,
+                                    "notas": 0,
+                                }
+                            },
+                        ],
+                        "as": "resposta",
+                    }
+                },
+                {"$set": {"resposta": {"$first": "$resposta._id"}}},
+                {
+                    "$project": {
+                        "_id": 1,
+                        "id_form": 0,
+                        "empresa": 0,
+                        "participantes": 0,
+                    }
+                },
+            ]
+        )
+        if r.alive:
+            return r.next()
+        else:
+            return None
