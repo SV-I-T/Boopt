ade2292b3942dfb67a61f91879448f1f83841ef1
Marco <marco@sucessoemvendas.com.br>
2024-03-06 15:01:53 -0300

    pagina inicial do assessment

--- src/pages/assessment_vendedores/assessment_vendedores.py
+++ src/pages/assessment_vendedores/assessment_vendedores.py
@@ -2,6 +2,7 @@ import dash_mantine_components as dmc
 from dash import register_page
 from flask_login import current_user
 from utils.banco_dados import db
+from utils.modelo_assessment import AssessmentVendedor
 from utils.modelo_usuario import Usuario, checar_login
 
 register_page(
@@ -14,4 +15,29 @@ register_page(
 
 @checar_login
 def layout():
-    return [dmc.Title("Bem-vindo ao Assessment de Vendedor")]
+    usr_atual: Usuario = current_user
+
+    aplicacao = AssessmentVendedor.buscar_ultimo(usr_atual.id_)
+
+    if aplicacao is not None:
+        if "resposta" in aplicacao:
+            BOTAO_TESTE = dmc.Button("Sem teste disponível", disabled=True)
+            BOTAO_RESULTADO = dmc.Anchor(
+                children=dmc.Button("Ver resultado"),
+                href=f"/assessment-vendedor/resultado/?id={aplicacao['resposta']}",
+            )
+        else:
+            BOTAO_TESTE = dmc.Anchor(
+                href=f"/assessment-vendedor/teste/?id={aplicacao['_id']}",
+                children=dmc.Button("Começar o teste"),
+            )
+            BOTAO_RESULTADO = dmc.Button("Ver resultado", disabled=True)
+    else:
+        BOTAO_TESTE = dmc.Button("Sem teste disponível", disabled=True)
+        BOTAO_RESULTADO = dmc.Button("Ver resultado", disabled=True)
+
+    return [
+        dmc.Title("Bem-vindo ao Assessment de Vendedor", order=1, weight=700),
+        BOTAO_TESTE,
+        BOTAO_RESULTADO,
+    ]
