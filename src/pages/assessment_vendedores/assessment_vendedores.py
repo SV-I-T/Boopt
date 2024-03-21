3d65b2a92f5047ed93de04cde24f0808a2375952
Marco <marco@sucessoemvendas.com.br>
2024-03-21 16:49:00 -0300

    tamanho dos elementos reduzidos

--- src/pages/assessment_vendedores/assessment_vendedores.py
+++ src/pages/assessment_vendedores/assessment_vendedores.py
@@ -25,51 +25,52 @@ def layout():
         align="center",
         justify="center",
         px="1rem",
+        h="100%",
         children=[
             dmc.Text(
                 "Bem-vindo ao Assessment de Vendedor",
-                size=64,
+                size=40,
                 weight=600,
                 align="center",
                 mb="1rem",
             ),
             dmc.Text(
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ligula arcu, auctor vel tellus vitae, aliquam dignissim quam.",
-                size=40,
                 weight=500,
                 align="center",
                 mb="4rem",
             ),
-            dmc.Anchor(
-                dmc.Button(
-                    children=dmc.Text(
-                        "Começar o teste"
+            dmc.Group(
+                children=[
+                    dmc.Anchor(
+                        dmc.Button(
+                            children=dmc.Text(
+                                "Começar o teste"
+                                if ultima_aplicacao
+                                and not ultima_aplicacao.get("resposta")
+                                else "Sem teste disponível",
+                                weight=400,
+                            ),
+                            disabled=not (
+                                ultima_aplicacao
+                                and not ultima_aplicacao.get("resposta")
+                            ),
+                            color="BooptLaranja",
+                        ),
+                        href=f"/app/assessment-vendedor/teste/?id={ultima_aplicacao['id']}"
                         if ultima_aplicacao and not ultima_aplicacao.get("resposta")
-                        else "Sem teste disponível",
-                        size=20,
-                        weight=400,
+                        else None,
                     ),
-                    disabled=not (
-                        ultima_aplicacao and not ultima_aplicacao.get("resposta")
+                    dmc.Anchor(
+                        dmc.Button(
+                            children=dmc.Text("Ver resultado", weight=400),
+                            disabled=not ultima_resposta_id,
+                        ),
+                        href=f"/app/assessment-vendedor/resultado/?usr={usr_atual.id}&resposta={ultima_resposta_id}"
+                        if ultima_resposta_id
+                        else None,
                     ),
-                    w=300,
-                    h=50,
-                    color="BooptLaranja",
-                ),
-                href=f"/app/assessment-vendedor/teste/?id={ultima_aplicacao['id']}"
-                if ultima_aplicacao and not ultima_aplicacao.get("resposta")
-                else None,
-            ),
-            dmc.Anchor(
-                dmc.Button(
-                    children=dmc.Text("Ver resultado", size=20, weight=400),
-                    disabled=not ultima_resposta_id,
-                    w=300,
-                    h=50,
-                ),
-                href=f"/app/assessment-vendedor/resultado/?usr={usr_atual.id}&resposta={ultima_resposta_id}"
-                if ultima_resposta_id
-                else None,
+                ]
             ),
         ],
     )
