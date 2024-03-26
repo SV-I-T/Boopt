5724d8b2dec11aaad34002cdfcc52aac032eb5c3
Marco <marco@sucessoemvendas.com.br>
2024-03-26 14:51:48 -0300

    pagina 404 atualizada

--- src/pages/not_found_404.py
+++ src/pages/not_found_404.py
@@ -1,14 +1,19 @@
 import dash_mantine_components as dmc
-from dash import html, register_page
+from dash import get_asset_url, html, register_page
 
 register_page(__name__, title="Página não encontrada")
 
-layout = [
-    dmc.Title(children="Ops!", order=1),
-    dmc.Title(children="Parece que essa página não existe.", order=3, weight=500),
-    dmc.Anchor(
-        children="Clique aqui para voltar à página inicial",
-        href="/",
-        underline=True,
-    ),
-]
+layout = html.Div(
+    className="center-container not-found",
+    children=[
+        dmc.Title(children="OPS...", order=1, weight=600),
+        html.Img(src=get_asset_url("imgs/404.png")),
+        dmc.Title(children="Parece que essa página não existe.", order=3, weight=400),
+        dmc.Anchor(
+            children=dmc.Button("Voltar à página inicial"),
+            href="/app/dashboard",
+            underline=True,
+            refresh=True,
+        ),
+    ],
+)
