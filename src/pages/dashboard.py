3da91b697a24c8bfc75b676f0af19220bbc77440
Marco <marco@sucessoemvendas.com.br>
2024-03-26 14:52:05 -0300

    adicionado redirect /app para /app/dashboard

--- src/pages/dashboard.py
+++ src/pages/dashboard.py
@@ -3,7 +3,9 @@ from dash import register_page
 from flask_login import current_user
 from utils.modelo_usuario import Usuario, checar_login
 
-register_page(__name__, path="/app/dashboard", title="Dashboard")
+register_page(
+    __name__, path="/app/dashboard", title="Dashboard", redirect_from=["/app"]
+)
 
 
 @checar_login
