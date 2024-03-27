3c6ceb7d83dc0901f3fd6f8dc22fefb2b8c7a0e9
Marco <marco@sucessoemvendas.com.br>
2024-03-27 15:45:54 -0300

    pagina de editar usuarios relayout

--- src/pages/admin/usuarios/usuarios_editar.py
+++ src/pages/admin/usuarios/usuarios_editar.py
@@ -14,17 +14,7 @@ from utils.modelo_usuario import CARGOS_PADROES, NovoUsuario, Usuario, checar_lo
 register_page(__name__, path="/app/admin/usuarios/edit", title="Editar usuário")
 
 
-def layout_edicao_usr(
-    nome: str = None,
-    sobrenome: str = None,
-    cpf: str = None,
-    data: str = None,
-    email: str = None,
-    empresa: ObjectId = None,
-    cargo: str = None,
-    gestor: bool = False,
-    recruta: bool = False,
-):
+def layout_edicao_usr(usr: Usuario = None):
     usr_atual: Usuario = current_user
 
     data_empresas = [
@@ -32,43 +22,34 @@ def layout_edicao_usr(
         for empresa in usr_atual.buscar_empresas()
     ]
 
-    if current_user.admin and nome is not None:
-        botao_excluir = dmc.Button(
-            id="btn-excluir-usr", children="Excluir", color="red", variant="outline"
-        )
-    else:
-        botao_excluir = None
-
     return html.Div(
-        style={"maxWidth": 600},
+        className="editar-usr",
         children=[
-            dmc.Group(
-                grow=True,
+            dmc.SimpleGrid(
+                cols=2,
+                breakpoints=[
+                    {"maxWidth": 567, "cols": 1},
+                ],
                 children=[
                     dmc.TextInput(
-                        id="nome-novo-usr",
-                        label="Primeiro Nome",
+                        id="nome-edit-usr",
+                        label="Nome",
                         type="text",
                         required=True,
                         icon=DashIconify(icon="fluent:person-24-filled", width=24),
                         name="nome",
-                        value=nome,
+                        value=usr.nome if usr else None,
                     ),
                     dmc.TextInput(
-                        id="sobrenome-novo-usr",
+                        id="sobrenome-edit-usr",
                         label="Sobrenome",
                         type="text",
                         required=True,
                         name="sobrenome",
-                        value=sobrenome,
+                        value=usr.sobrenome if usr else None,
                     ),
-                ],
-            ),
-            dmc.Group(
-                grow=True,
-                children=[
                     dmc.TextInput(
-                        id="cpf-novo-usr",
+                        id="cpf-edit-usr",
                         label="CPF",
                         required=True,
                         description="Somente números",
@@ -77,10 +58,10 @@ def layout_edicao_usr(
                             icon="fluent:slide-text-person-24-filled", width=24
                         ),
                         name="cpf",
-                        value=cpf,
+                        value=usr.cpf if usr else None,
                     ),
                     dmc.DatePicker(
-                        id="data-novo-usr",
+                        id="data-edit-usr",
                         label="Data de Nascimento",
                         required=True,
                         description="Será a senha do usuário",
@@ -94,27 +75,21 @@ def layout_edicao_usr(
                             icon="fluent:calendar-date-24-filled", width=24
                         ),
                         name="data",
-                        value=datetime.strptime(data, "%Y-%m-%d").date()
-                        if data is not None
+                        value=datetime.strptime(usr.data, "%Y-%m-%d").date()
+                        if usr
                         else None,
                     ),
-                ],
-            ),
-            dmc.Group(
-                grow=True,
-                align="end",
-                children=[
                     dmc.TextInput(
-                        id="email-novo-usr",
+                        id="email-edit-usr",
                         label="E-mail",
                         type="email",
                         placeholder="nome@dominio.com",
                         icon=DashIconify(icon="fluent:mail-24-filled", width=24),
                         name="email",
-                        value=email,
+                        value=usr.email if usr else None,
                     ),
                     dmc.Select(
-                        id="cargo-novo-usr",
+                        id="cargo-edit-usr",
                         label="Cargo/Função",
                         required=True,
                         description="Selecione a opção que melhor se encaixa ao cargo",
@@ -126,16 +101,10 @@ def layout_edicao_usr(
                             icon="fluent:person-wrench-20-filled", width=24
                         ),
                         name="cargo",
-                        value=cargo,
+                        value=usr.cargo if usr else None,
                     ),
-                ],
-            ),
-            dmc.Group(
-                grow=True,
-                align="center",
-                children=[
                     dmc.Select(
-                        id="empresa-novo-usr",
+                        id="empresa-edit-usr",
                         label="Empresa",
                         icon=DashIconify(icon="fluent:building-24-filled", width=24),
                         name="empresa",
@@ -143,24 +112,31 @@ def layout_edicao_usr(
                         required=True,
                         searchable=True,
                         nothingFound="Não encontrei nada",
-                        value=str(empresa) if empresa is not None else None,
+                        value=str(usr.empresa) if usr else None,
                     ),
-                    dmc.Switch(
-                        id="gestor-novo-usr",
-                        label="Permissão de Gestão",
-                        checked=gestor,
+                    html.Div(
+                        [
+                            dmc.Switch(
+                                id="gestor-edit-usr",
+                                label="Permissões de gestão da empresa",
+                                checked=usr.gestor if usr else False,
+                            ),
+                            dmc.Switch(
+                                id="recruta-edit-usr",
+                                label="Este usuário é um candidato",
+                                checked=usr.recruta if usr else False,
+                            ),
+                        ]
                     ),
                 ],
             ),
-            dmc.Checkbox(
-                id="recruta-novo-usr", label="Seleção e Recrutamento", checked=recruta
-            ),
             dmc.Button(
-                id="btn-criar-novo-usr" if nome is None else "btn-salvar-usr",
-                children="Criar" if nome is None else "Salvar",
+                id="btn-salvar-usr" if usr else "btn-criar-usr",
+                children="Salvar" if usr else "Criar",
             ),
-            botao_excluir,
-            html.Div(id="feedback-novo-usr"),
+            dmc.Button(id="btn-excluir-usr", children="Excluir", color="red")
+            if usr_atual.admin and usr is not None
+            else None,
         ],
     )
 
@@ -176,19 +152,9 @@ def layout(id: str = None):
             DashIconify(icon="fluent:edit-28-filled", width=28, color="#777"),
             f"{usr.nome} {usr.sobrenome}",
         ]
-        layout_edicao = layout_edicao_usr(
-            usr.nome,
-            usr.sobrenome,
-            usr.cpf,
-            usr.data,
-            usr.email,
-            usr.empresa,
-            usr.cargo,
-            usr.gestor,
-            usr.recruta,
-        )
+        layout_edicao = layout_edicao_usr(usr)
     return [
-        dmc.Title(texto_titulo, order=1, weight=700),
+        dmc.Title(texto_titulo, className="titulo-pagina"),
         layout_edicao,
     ]
 
@@ -196,15 +162,15 @@ def layout(id: str = None):
 @callback(
     Output("notificacoes", "children", allow_duplicate=True),
     Input("btn-salvar-usr", "n_clicks"),
-    State("nome-novo-usr", "value"),
-    State("sobrenome-novo-usr", "value"),
-    State("cpf-novo-usr", "value"),
-    State("data-novo-usr", "value"),
-    State("email-novo-usr", "value"),
-    State("empresa-novo-usr", "value"),
-    State("cargo-novo-usr", "value"),
-    State("gestor-novo-usr", "checked"),
-    State("recruta-novo-usr", "checked"),
+    State("nome-edit-usr", "value"),
+    State("sobrenome-edit-usr", "value"),
+    State("cpf-edit-usr", "value"),
+    State("data-edit-usr", "value"),
+    State("email-edit-usr", "value"),
+    State("empresa-edit-usr", "value"),
+    State("cargo-edit-usr", "value"),
+    State("gestor-edit-usr", "checked"),
+    State("recruta-edit-usr", "checked"),
     State("url", "search"),
     prevent_initial_call=True,
 )
@@ -271,16 +237,16 @@ def salvar_usr(
 
 @callback(
     Output("notificacoes", "children", allow_duplicate=True),
-    Input("btn-criar-novo-usr", "n_clicks"),
-    State("nome-novo-usr", "value"),
-    State("sobrenome-novo-usr", "value"),
-    State("cpf-novo-usr", "value"),
-    State("data-novo-usr", "value"),
-    State("email-novo-usr", "value"),
-    State("empresa-novo-usr", "value"),
-    State("cargo-novo-usr", "value"),
-    State("gestor-novo-usr", "checked"),
-    State("recruta-novo-usr", "checked"),
+    Input("btn-criar-usr", "n_clicks"),
+    State("nome-edit-usr", "value"),
+    State("sobrenome-edit-usr", "value"),
+    State("cpf-edit-usr", "value"),
+    State("data-edit-usr", "value"),
+    State("email-edit-usr", "value"),
+    State("empresa-edit-usr", "value"),
+    State("cargo-edit-usr", "value"),
+    State("gestor-edit-usr", "checked"),
+    State("recruta-edit-usr", "checked"),
     prevent_initial_call=True,
 )
 def criar_novo_usr(
@@ -349,12 +315,12 @@ def criar_novo_usr(
     State("url", "search"),
     prevent_initial_call=True,
 )
-def abrir_modal_excluir_usr(n: int, botao: str, search: str):
+def confirmar_exclusao_usr(n: int, botao: str, search: str):
     if not n:
         raise PreventUpdate
 
     if botao == "Excluir":
-        BOTAO = "Confirmar exclusão"
+        BOTAO = "Confirmar"
         HREF = no_update
     else:
         params = parse_qs(search[1:])
