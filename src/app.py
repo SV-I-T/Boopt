976f801267905226e489eafb2de267da10d88285
Marco <marco@sucessoemvendas.com.br>
2024-09-03 13:53:03 -0300

    cor do tema mudada para branco

--- src/app.py
+++ src/app.py
@@ -32,7 +32,7 @@ app = Dash(
     external_stylesheets=[
         "https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap"
     ],
-    meta_tags=[{"name": "theme-color", "content": "#171C24"}],
+    meta_tags=[{"name": "theme-color", "content": "#ffffff"}],
 )
 auth = BasicAuth(
     app=app,
