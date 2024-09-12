386820b7075c6376968aebf058c8aa5e73baa138
Marco <marco@sucessoemvendas.com.br>
2024-09-12 11:20:34 -0300

    removido log de debug

--- src/static/js/funcoes-dag.js
+++ src/static/js/funcoes-dag.js
@@ -2,7 +2,6 @@ var dagfuncs = window.dashAgGridFunctions = window.dashAgGridFunctions || {};
 
 
 dagfuncs.FMT = function (number) {
-    console.log(number);
     if (typeof number === 'number') {
         return number.toLocaleString('pt-br')
     }
