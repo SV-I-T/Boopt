76ca335e9a7becd5b8474ecd93d4206dadb7dd1b
Marco <marco@sucessoemvendas.com.br>
2024-11-29 13:24:10 -0300

    só pode ver a galeria de vídeos se tiver feito um assessment ou for RH

--- pages/vela/videos_gallery.py
+++ pages/vela/videos_gallery.py
@@ -1,11 +1,27 @@
 from dash import dcc, get_asset_url, html, register_page
 
+from utils import Role, Usuario, db
 from utils.video import video_teste
 
 register_page(__name__, path="/app/videos/vela", title="Biblioteca de Videos - Vela")
 
 
 def layout():
+    usr = Usuario.atual()
+
+    resposta = db("VelaRespostas").find_one({"id_usuario": usr.id_}, {"_id": 1})
+
+    if not resposta and usr.role in (Role.USR, Role.GEST):
+        return html.H1(
+            [
+                "Vídeos",
+                html.Img(src=get_asset_url("assets/vela/tag_ass.svg")),
+            ],
+            html.H2(
+                "Você precisa primeiro ter um Assessment Vela completado para pode assistir aos vídeos."
+            ),
+        )
+
     return [
         html.H1(
             [
